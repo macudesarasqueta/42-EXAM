d7d1c282145b8befdc440885b334e231d5b3d87f
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-02-27 12:32:12 +0100

    ok

--- Level 2/inter/inter.c
+++ Level 2/inter/inter.c
@@ -2,16 +2,22 @@
 
 int	main(int argc, char **argv)
 {
-	int	i = 0;
-	int	j = 0;
+	int used[255] = {0};
 
 	if (argc == 3)
 	{
+		int i = 0;
 		while (argv[1][i])
 		{
-			while (argv[1][i] == argv[2][j])
+			int j = 0;
+			while (argv[2][j])
 			{
-				write(1, &argv[1][i], 1);
+				if (argv[1][i] == argv[2][j] && !used[(int)argv[1][i]])
+				{
+					write(1, &argv[1][i], 1);
+					used[(int)argv[1][i]] = 1;
+					break ;
+				}
 				j++;
 			}
 			i++;
