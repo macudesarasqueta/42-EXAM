44fa15dc48fc3d294c490c0ce5c0447338552cc1
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-01 13:06:27 +0100

    Update ft_range.c

--- Level 3/ft_range/ft_range.c
+++ Level 3/ft_range/ft_range.c
@@ -1,40 +1,43 @@
 #include <stdlib.h>
-#include <unistd.h>
 
-int     *ft_range(int start, int end)
+int *ft_range(int start, int end)
 {
-	int	*range;
-	int	i = 0;
-
-	if (start > end)
-		range = (int *)malloc(sizeof(int) * (start - end) + 1);
-	else
-		range = (int *)malloc(sizeof(int) * (end - start) + 1);
-	while (start < end)
+	int i = 0;
+	int len = abs((end - start)) + 1;
+	int *res = (int *)malloc(sizeof(int) * len);
+	
+	while (i < len)
 	{
-		range[i] = start;
-		write(1, &range[i], 1);
-		start++;
-		i++;
-	}	
-	range[i] = start;
-	return (range);
+		if (start < end)
+		{
+			res[i] = start;
+			start++;
+			i++;
+		}
+		else
+		{
+			res[i] = start;
+			start--;
+			i++;
+		}
+	}
+        return (res);
 }
-#include <stdio.h>
 
+/*#include <stdio.h>
 
-/*void	print_array(int *array, int size)
+void	print_array(int *array, int size)
 {
 	for (int i = 0; i < size; i++)
 		printf("%d ", array[i]);
 	printf("\n");
-}*/
+}
 
 int	main(void)
 {
-	int start = 1;
+	int start = 0;
 	int end = 3;
-	ft_range(start, end);
-	//free(range_range);
+    	print_array(ft_range(start, end), 4);
+	free(ft_range(start, end));
 	return (0);
-}
+}*/
