47f5c4bf4054038d1afce1d6e37ccc0f34752795
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-04 14:41:23 +0100

    Update paramsum.c

--- Level 3/paramsum/paramsum.c
+++ Level 3/paramsum/paramsum.c
@@ -9,19 +9,6 @@ void ft_putnbr(int n)
 	write(1, &digit, 1);
 }
 
-/*int	main(int argc, char **argv)
-{
-	if (argc > 1)
-	{
-		if (argc)
-			ft_putnbr(argc - 1);
-	}
-	else
-		write(1, "0", 1);
-	write(1, "\n", 1);
-	return (0);
-}*/
-
 int	main(int argc, char **argv)
 {
 	ft_putnbr(argc - 1);
