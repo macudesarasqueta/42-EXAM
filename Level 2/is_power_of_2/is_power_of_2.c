7d2100f09d320be059d88c0c7e089e6bbdba91c1
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-04 12:05:35 +0100

    Update is_power_of_2.c

--- Level 2/is_power_of_2/is_power_of_2.c
+++ Level 2/is_power_of_2/is_power_of_2.c
@@ -2,8 +2,26 @@ int	    is_power_of_2(unsigned int n)
 {
 	if (n == 0)
 		return (0);
-	else if (n % 2 == 0)
+	if (n == 1)
 		return (1);
-	else
-		return (0);
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+		if (n == 1)
+			return (1);
+	}
+	return (0);
 }
+
+/*#include <stdio.h>
+
+int main()
+{
+	int n = 1;
+	int n0 = 0;
+	int n1 = 2;
+	printf("num = 1: %d\n", is_power_of_2(n));
+	printf("num = 0: %d\n", is_power_of_2(n0));
+	printf("num = 1: %d\n", is_power_of_2(n1));
+	return (0);
+}*/
