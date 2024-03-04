2790b111e14a87ebba2a4157d7f437935e61e6cc
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-04 14:24:35 +0100

    Add files via upload

---
+++ Level 2/max/max.c
@@ -0,0 +1,25 @@
+int		max(int* tab, unsigned int len)
+{
+	unsigned int res;
+	unsigned i = 0;
+
+	if (len == 0)
+		return (0);
+	res = tab[i];
+	while (len > i)
+	{
+		if (res < tab[i])
+			res = tab[i];
+		i++;
+	}
+	return (res);
+}
+
+/*#include <stdio.h>
+
+int main()
+{
+	int num[] = {1, 3, 8, 0};
+	printf("%d\n", max(num, 4));
+	return (0);
+}*/
