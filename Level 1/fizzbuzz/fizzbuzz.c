bc6c991dbe49e30dbc6491cded29edfde8efe39a
Macarena De Sarasqueta Szneiderowicz <mde-sara@car9s5.42barcelona.com>
2024-03-11 18:02:29 +0100

    update fizzbuzz

--- Level 1/fizzbuzz/fizzbuzz.c
+++ Level 1/fizzbuzz/fizzbuzz.c
@@ -1,28 +1,34 @@
 #include <unistd.h>
 
-void write_n(int num)
+void	ft_putnbr(int num)
 {
-	char str[10] = "0123456789";
-
+	char	digit;
 	if (num >= 10)
-		write_n(num / 10);
-	write (1, &str[num % 10], 1);
+		ft_putnbr(num / 10);
+	digit = (num % 10) + '0';
+	write(1, &digit, 1);
 }
 
-int	main(void)
+void	fizzbuzz(int num)
 {
-	int n;
-	while (n >= 1 && n <= 100)
+	if (num % 3 == 0 && num % 5 == 0)
+		write(1, "fizzbuzz", 8);
+	else if (num % 3 == 0)
+		write(1, "fizz", 4);
+	else if (num % 5 == 0)
+		write(1, "buzz", 4);
+	else
+		ft_putnbr(num);
+}
+
+int main()
+{
+	int num = 1;
+
+	while (num <= 100)
 	{
-		if (n % 3 == 0 && n % 5 == 0)
-			write(1, "fizzbuzz", 8);
-		else if (n % 3 == 0)
-			write(1, "fizz", 4);
-		else if (n % 5 == 0)
-			write(1, "buzz", 4);
-		else
-			write_n(n);
-		n++;
+		fizzbuzz(num);
+		num++;
 		write(1, "\n", 1);
 	}
 	return (0);
