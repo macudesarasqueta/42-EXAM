994310aed6e0d486aef41439d0f37ed13d622124
Macarena De Sarasqueta Szneiderowicz <mde-sara@car9s5.42barcelona.com>
2024-03-11 12:13:04 +0100

    printf_bits

---
+++ Level 2/print_bits/print_bits.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void print_bits(unsigned char octet)
+{
+	int	i = 8;
+	unsigned char 	bit;
+
+	while (i--)
+	{
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
+	}
+}
+
+#include <stdio.h>
+
+int main()
+{
+	print_bits(2);
+	return (0);
+}
