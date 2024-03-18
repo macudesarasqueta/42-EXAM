0d234e1e1906448859c56f7737cb4d1b23b95502
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-18 19:06:17 +0100

    Update is_power_of_2.c

--- Level 2/is_power_of_2/is_power_of_2.c
+++ Level 2/is_power_of_2/is_power_of_2.c
@@ -2,15 +2,12 @@ int	    is_power_of_2(unsigned int n)
 {
 	if (n == 0)
 		return (0);
-	if (n == 1)
+	else if (n == 1)
 		return (1);
-	while (n % 2 == 0)
-	{
-		n = n / 2;
-		if (n == 1)
-			return (1);
-	}
-	return (0);
+	else if (n % 2 == 0)
+		return (1);
+	else
+		return (0);
 }
 
 /*#include <stdio.h>
