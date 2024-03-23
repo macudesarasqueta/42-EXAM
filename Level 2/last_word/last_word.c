497741aea138401f2e66e92989e9bae7668b0750
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-23 11:48:37 +0100

    Update last_word.c

--- Level 2/last_word/last_word.c
+++ Level 2/last_word/last_word.c
@@ -5,30 +5,26 @@ int	main(int argc, char **argv)
 {
 	int	i = 0;
 	int	start;
-	int	start2;
 	int	end;
 
 	if (argc == 2)
 	{
 		while (argv[1][i] == ' ' || argv[1][i] == '\t')
 			i++;
-		start = i;
 		while (argv[1][i])
 			i++;
 		i--;
 		while (i >= 0 && (argv[1][i] == ' ' || argv[1][i] == '\t'))
 			i--;
 		end = i;
-		//printf("end = %d\n", end);
 		while (i >= 0 && argv[1][i] != ' ' && argv[1][i] != '\t')
 			i--;
 		i++;
-		start2 = i;
-		//printf("start2 = %d\n", start2);
-		while (start2 <= end)
+		start = i;
+		while (start <= end)
 		{
-			write(1, &argv[1][start2], 1);
-			start2++;
+			write(1, &argv[1][start], 1);
+			start++;
 		}
 	}
 	write(1, "\n", 1);
