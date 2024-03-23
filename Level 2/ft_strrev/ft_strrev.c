90a435b594e3c85fac7c6f72304b8cec3cbed233
Macarena De Sarasqueta Szneiderowicz <mde-sara@car4s6.42barcelona.com>
2024-03-23 11:34:52 +0100

    strrev

---
+++ Level 2/ft_strrev/ft_strrev.c
@@ -0,0 +1,29 @@
+char *ft_strrev(char *str)
+{
+	int i = -1;
+	int len = 0;
+	char temp;
+
+	while (str[len])
+		len++;
+	len--;
+	while (i < len)
+	{
+		temp = str[i];
+		str[i] = str[len];
+		str[len] = temp;
+		i++;
+		len--;
+	}
+	return (str);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char s[] = "Hello World";
+	ft_strrev(s);
+	printf("%s\n", s);
+	return (0);
+}*/
