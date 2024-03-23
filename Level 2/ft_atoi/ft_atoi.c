9eaee47a5f656fb3e06097d5e99909b39945685b
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-23 11:04:19 +0100

    Update ft_atoi.c

--- Level 2/ft_atoi/ft_atoi.c
+++ Level 2/ft_atoi/ft_atoi.c
@@ -8,7 +8,7 @@ int	ft_atoi(const char *str)
 		i++;
 	if (str[i] == '-')
 		sign = -1;
-	if (str[i] == '-' || str[i] == '+')
+	while (str[i] == '-' || str[i] == '+')
 		i++;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
