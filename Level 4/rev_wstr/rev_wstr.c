7622d1001fd69c113f9281e7eeda42d46cdede5e
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-25 18:46:12 +0100

    Update rev_wstr.c

--- Level 4/rev_wstr/rev_wstr.c
+++ Level 4/rev_wstr/rev_wstr.c
@@ -1,31 +1,34 @@
-#include <unistd.h> 
+#include <unistd.h>
 
-void rev_wstr(char *str) 
-{ 
-        int i = 0;
-        int j = 0;
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int start;
+	int end;
 
-        while (str[i]) 
-                i++; 
-        i--; 
-        while (i >= 0) 
-        { 
-                while (str[i] > 32 && str[i] < 127) 
-                        i--; 
-                j = i + 1; 
-                while (str[j] > 32 && str[j] < 127) 
-                { 
-                        write(1, &str[j], 1); 
-                        j++; 
-                } 
-                if (str[i] == 32) 
-                { 
-                        write(1, " ", 1); 
-                        i--; 
-                } 
-        } 
+	if (argc == 2)
+	{
+		while (argv[1][i])
+			i++;
+		while (i >= 0)
+		{
+			end = i;
+			while (argv[1][i] > 32 && argv[1][i] < 127)
+				i--;
+			start = i + 1;
+			while (start <= end)
+			{
+				write(1, &argv[1][start], 1);
+				start++;
+			}
+			if (argv[1][i] == ' ')
+				write(1, " ", 1);
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
-
 int     main(int argc, char **argv) 
 { 
         if (argc == 2) 
