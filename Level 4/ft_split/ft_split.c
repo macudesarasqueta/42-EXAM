bb8efec83f141576379e2bc1cd4d426809ee7611
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-25 15:55:26 +0100

    Update ft_split.c

--- Level 4/ft_split/ft_split.c
+++ Level 4/ft_split/ft_split.c
@@ -28,3 +28,38 @@ char    **ft_split(char *str)
 	split[k] = NULL;
 	return (split);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+	int i;
+	char str[] = "Hello world   this is a test";
+	char **result = ft_split(str);
+
+    	if (result == NULL) 
+	{
+        	printf("Error: No se pudo asignar memoria.\n");
+        	return (1);
+    	}
+
+	i = 0;
+    	while (result[i++])
+        	printf("[%d]: %s\n", i, result[i]);
+
+    	// Liberar la memoria asignada
+	i = 0;   
+	while (result[i++])
+        	free(result[i]);
+    	free(result);
+	return (0);
+}
+
+//output
+[1]: world
+[2]: this
+[3]: is
+[4]: a
+[5]: test
+[6]: (null)
