f9a72b88efba8d34018f469f72490ad23ec5c18b
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-03-30 13:08:24 +0100

    Update sort_int_tab.c

--- Level 4/sort_int_tab/sort_int_tab.c
+++ Level 4/sort_int_tab/sort_int_tab.c
@@ -1,15 +1,14 @@
 void sort_int_tab(int *tab, unsigned int size)
 {
-	unsigned int i = 0;
-	int	temp;
-
-	while (i < (size - 1))
+	int i = 0;
+	int swap;
+	while (i < size - 1)
 	{
 		if (tab[i] > tab[i + 1])
 		{
-			temp = tab[i];
-			tab[i] = tab[i + 1];
-			tab[i + 1] = temp;
+			swap = tab[i + 1];
+			tab[i + 1] = tab[i];
+			tab[i] = swap;
 			i = 0;
 		}
 		else
@@ -17,11 +16,11 @@ void sort_int_tab(int *tab, unsigned int size)
 	}
 }
 
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int tab[] = {5, 7, 8, 3};
+	int tab[] = {9, 7, -7, 0};
 	int size = 4;
 	sort_int_tab(tab, size);
 	unsigned int i = 0;
@@ -31,4 +30,4 @@ int main()
 		i++;
 	}
 	return (0);
-}*/
+}
