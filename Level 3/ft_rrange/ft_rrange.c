218fb526b8c253f5778452908ff8f1b4a9929fc2
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-04-11 12:38:35 +0200

    Update ft_rrange.c

--- Level 3/ft_rrange/ft_rrange.c
+++ Level 3/ft_rrange/ft_rrange.c
@@ -25,3 +25,37 @@ int *ft_rrange(int start, int end)
 	}
 	return (range);
 }
+
+/*#include <stdio.h>
+
+int main() 
+{
+    int start = 1;
+    int end = 5;
+
+    // Llamada a la función ft_rrange
+    int *result = ft_rrange(start, end);
+
+    // Imprimir el resultado
+    if (result) 
+    {
+        // int i = 0;
+        // int n = end - start;
+
+        // if (n < 0)
+        //     n = -n;
+        // n++;
+        while (i < n) 
+	{
+            printf("%d ", result[i]);
+            i++;
+        }
+        printf("\n");
+
+        // Liberar la memoria asignada
+        free(result);
+    } 
+    else
+        printf("Error: no se pudo asignar memoria.\n");
+    return (0);
+}*/
