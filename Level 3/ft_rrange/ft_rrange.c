60a527b82bb7afef33942f458a67f06b01162707
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-04-11 12:24:41 +0200

    Update ft_rrange.c

--- Level 3/ft_rrange/ft_rrange.c
+++ Level 3/ft_rrange/ft_rrange.c
@@ -8,7 +8,7 @@ int *ft_rrange(int start, int end)
 	int n = end - start;
 
 	if (n < 0)
-		(n = n* (-1));
+		n = -n;
 	n++;
 
 	range = (int *)malloc(sizeof(int) * n);
@@ -25,27 +25,3 @@ int *ft_rrange(int start, int end)
 	}
 	return (range);
 }
-
-/*#include <stdio.h>
-
-int main() 
-{
-    int start = 0;
-    int end = -3;
-    int *result = ft_rrange(start, end);
-    
-	if (result == NULL) 
-	{
-        printf("Error: No se pudo asignar memoria.\n");
-        return 1;
-    }
-
-    printf("Resultado:\n");
-    for (int i = 0; i < end - start + 1; i++) {
-        printf("%d ", result[i]);
-    }
-    printf("\n");
-
-    free(result);
-    return 0;
-}*/
