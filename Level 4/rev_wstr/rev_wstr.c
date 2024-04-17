d7ea0f5671b47fa77b261fa2802dcc49007f40fe
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-04-17 13:12:39 +0200

    Update rev_wstr.c

--- Level 4/rev_wstr/rev_wstr.c
+++ Level 4/rev_wstr/rev_wstr.c
@@ -1,38 +1,91 @@
 #include <unistd.h>
+#include <stdlib.h>
+
+int	is_space(char c)
+{
+	if (c == ' ')
+		return (1);
+	if (c == '\t')
+		return (1);
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	count_words(char *str)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (i == 0 && !is_space(str[i]))
+			count++;
+		if (i > 0 && !is_space(str[i]) && is_space(str[i - 1]))
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char    **ft_split(char *str)
+{
+	char **mem;
+	int i = 0;
+	int j;
+	int k = 0;
+
+	mem = (char **)malloc(sizeof(char) * count_words(str) + 1);
+	if (!mem)
+		return (NULL);
+	while (is_space(str[i]))
+		i++;
+	while (str[i])
+	{
+		j = 0;
+		mem[k] = (char *)malloc(sizeof(char) * 3000);
+		if (!mem[k])
+			return (NULL);
+		while (!is_space(str[i]) && str[i])
+			mem[k][j++] = str[i++];
+		while (is_space(str[i]))
+			i++;
+		mem[k][j] = '\0';
+		k++;
+	}
+	mem[k] = NULL;
+	return (mem);
+}
 
 int main(int argc, char **argv)
 {
-	int i = 0;
-	int start;
-	int end;
-
-	if (argc == 2)
+	if (argc >= 2)
 	{
-		while (argv[1][i])
-			i++;
-		while (i >= 0)
+		char **matrix;
+		int num_words;
+
+		matrix = ft_split(argv[1]);
+		num_words = count_words(argv[1]);
+		while (--num_words > 0)
 		{
-			end = i;
-			while (argv[1][i] > 32 && argv[1][i] < 127)
-				i--;
-			start = i + 1;
-			while (start <= end)
-			{
-				write(1, &argv[1][start], 1);
-				start++;
-			}
-			if (argv[1][i] == ' ')
-				write(1, " ", 1);
-			i--;
+			ft_putstr(matrix[num_words]);
+			write(1, " ", 1);
 		}
+        if (num_words == 0)
+		    ft_putstr(matrix[num_words]);
 	}
 	write(1, "\n", 1);
 	return (0);
-}
-int     main(int argc, char **argv) 
-{ 
-        if (argc == 2) 
-                rev_wstr(argv[1]); 
-        write(1, "\n", 1); 
-        return (0); 
 }
