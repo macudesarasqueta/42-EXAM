320e374edfc92a26af7ae3456844b050562f3ab0
Macudesara <133897006+Macudesara@users.noreply.github.com>
2024-04-24 18:00:43 +0200

    Update str_capitalizer.c

--- Level 3/str_capitalizer/str_capitalizer.c
+++ Level 3/str_capitalizer/str_capitalizer.c
@@ -1,35 +1,37 @@
 #include <unistd.h>
 
+int	is_space(char c)
+{
+	if (c == ' ' || c == '\t')
+		return (1);
+	return (0);
+}
+
 void	str_capitalizer(char *str)
 {
-	int	i = 0;
-
+	int i = 0;
 	while (str[i])
 	{
-		if	(str[i] >= 'A' && str[i] <= 'Z')
+		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] = str[i] + 32;
 		i++;
 	}
-
 	i = 0;
-	if (str[i] >= 'a' && str[i] <= 'z')
-	{
-		str[i] = str[i] - 32;
-		write(1, &str[i], 1);
-		i++;
-	}
 	while (str[i])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i - 1] == ' ' || str[i - 1] == '\t'))
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		else if (i > 0 && str[i] >= 'a' && str[i] <= 'z' && is_space(str[i - 1]))
 			str[i] = str[i] - 32;
 		write(1, &str[i], 1);
 		i++;
 	}
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	i = 1;
+	int i = 1;
+
 	while (argc > i)
 	{
 		str_capitalizer(argv[i]);
